aa55eea1b9a0d78902a2884e00c6127e476d53bd
SQL F <137733866+sql-is-life@users.noreply.github.com>
2023-07-02 14:13:25 +0100

    feat(CQRS projection patterns):

---
+++ models/v2/30 Marts/v2_mart_music_song_p1.sql
@@ -0,0 +1,10 @@
+SELECT      msp.SongId,
+            msp.ArtistName,
+            msp.AlbumName,
+            msp.SongName,
+            msp.SongLength,
+            msp.SongSizeMb,
+            msp.OccurredAt as Published_Date,
+            msr.OccurredAt as Removed_Date
+FROM {{ ref('v2_int_music_song_published') }} AS msp
+      LEFT OUTER JOIN FROM {{ ref('v2_int_music_song_removed') }} AS msr ON msp._id = msr._id
---
+++ models/v2/30 Marts/v2_mart_music_song_p2.sql
@@ -0,0 +1,11 @@
+-- CQRS Style Project 1 with LEFT OUTER JOIN
+SELECT      msp.SongId,
+            msp.ArtistName,
+            msp.AlbumName,
+            msp.SongName,
+            msp.SongLength,
+            msp.SongSizeMb,
+            msp.OccurredAt as Published_Date,
+            msr.OccurredAt as Removed_Date
+FROM {{ ref('v2_int_music_song_published') }} AS msp
+      LEFT OUTER JOIN FROM {{ ref('v2_int_music_song_removed') }} AS msr ON msp._id = msr._id
