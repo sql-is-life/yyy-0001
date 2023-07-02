37aae93b03b62e1ab02e6a3ef52ad5ecce47c1e6
SQL F <137733866+sql-is-life@users.noreply.github.com>
2023-07-02 10:58:45 +0100

    feat(v2): add staging layer for v2 model &  add intermediate layer for v2 model

---
+++ models/v2/10 Staging/v2_stg_music_song_removed.sql
@@ -0,0 +1,5 @@
+SELECT *
+FROM `z-terraform-1001-dev.yyy_0001_0_dev.v2_raw_music_song_removed`
+
+-- Example of using PARTITIONTIME
+-- WHERE TIMESTAMP_TRUNC(_PARTITIONTIME, DAY) = TIMESTAMP("2023-07-02")
---
+++ models/v2/20 Intermediate/v2_int_music_song_listened.sql
@@ -0,0 +1,32 @@
+-- v2_int_music_song_listened.sql
+with
+    eventlogs_music_song_listened_json_extracted as (
+        select
+            entityid as _id,
+            lastmodifiedtimestamp as updated_at,
+            json_extract_scalar(json, '$.DeviceId') as deviceid,
+            json_value(json, '$.DeviceId') as deviceid2
+        from {{ ref('v2_stg_music_song_listened') }}
+        where eventtypeid = 2
+    ),
+
+    eventlogs_music_song_listened_rolled_up as (
+        select
+            *, row_number() over (partition by _id order by updated_at desc) as sequence
+        from eventlogs_music_song_listened_json_extracted
+    )
+
+select distinct * except (sequence)
+from eventlogs_music_song_listened_rolled_up as s
+where sequence = 1
+
+
+/*
+This DBT model represents an intermediate view of our dataset.
+
+Objective:
+It aims to remove any duplicate records in our data, maintaining the uniqueness of each row. The uniqueness is determined based on the combination of all fields. If two rows have exactly the same values in all fields, one of them is considered as a duplicate and removed from this view.
+
+Partitioning:
+The data in this model is partitioned on BigQuery using the _PARTITIONTIME pseudo column. The granularity of the partition is one day, meaning that the data is divided into separate partitions for each day. This partitioning strategy optimizes queries that filter by date, as BigQuery only needs to scan the relevant partition(s) instead of the entire table.
+*/
