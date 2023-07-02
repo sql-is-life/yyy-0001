e26f58dd967c90822d9417cf5deef31229753775
SQL F <137733866+sql-is-life@users.noreply.github.com>
2023-07-02 15:39:58 +0100

    feat(mart): music listened

--- models/v2/30 Marts/v2_mart_music_by_song_p1.sql
+++ models/v2/30 Marts/v2_mart_music_by_song_p1.sql
@@ -6,6 +6,8 @@ SELECT      msp.SongId,
             msp.SongLength,
             msp.SongSizeMb,
             msp.OccurredAt as Published_Date,
+            msl.Count_Song_Played as Count_Song_Played, 
             msr.OccurredAt as Removed_Date
 FROM {{ ref('v2_int_music_song_published') }} AS msp
       LEFT OUTER JOIN  {{ ref('v2_int_music_song_removed') }} AS msr ON msp._id = msr._id
+      LEFT OUTER JOIN  {{ ref('v2_mart_music_listened_p1') }} AS msl ON msl.SongId = msp._id
